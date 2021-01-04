f981c564b4812b3d1fd852389022a5a6cf342343
Mehbub Rashid <mehbub.rabu@gmail.com>
2021-01-04 14:17:50 +0600

    added menu handler class

---
+++ plugin-name/admin/partials/class-plugin-name-menu-handler.php
@@ -0,0 +1,24 @@
+<?php 
+
+/**
+ * Class responsible for handling menu items and pages.
+ */
+class Plugin_Name_Menu_Handler {
+
+    /**
+     * Register all menu items.
+     *
+     * @return void
+     */
+    public function register_menu_items() {
+        // add_menu_page codes go here
+    }
+}
+
+
+
+
+
+
+
+?>
